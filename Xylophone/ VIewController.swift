4a4557557cd527b719a16e49802a701a90175c8b
Mohammad Agung <diditkazama@gmail.com>
2020-01-09 11:25:32 +0700

    update nama musik, tombol diberi animasi

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -10,27 +10,29 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate{
+    
     var audioPlayer: AVAudioPlayer!
-    let soundFile = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+//    let soundFile = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
-        playSound(sound: soundFile[sender.tag - 1])
+        playSound(sound: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
     }
     
     func playSound(sound: String) {
         let soundURL = Bundle.main.url(forResource: sound, withExtension: "wav")
         
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
-        }
-        catch {
-            print(error)
-        }
-        
+        audioPlayer = try! AVAudioPlayer(contentsOf: soundURL!)
+                
         audioPlayer.play()
     }
 }
